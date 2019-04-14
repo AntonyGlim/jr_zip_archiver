21ad914b408b9c3389a34a02c4df90d2438daf07
Antony.Glim <Antony.Glim@gmail.com>
2019-04-14 21:48:13 +0300

    13 getFilesList() method in class ZipFileManager add

--- FileProperties.java
+++ FileProperties.java
@@ -1,16 +1,10 @@
 package com.javarush.task.task31.task3110;
 
-/**
- * Класс призван отвечать за свойства каждого файла в архиве.
- * Свойства - это набор, состоящий из:
- * имя файла, размер файла до и после сжатия, метод сжатия.
- */
 public class FileProperties {
-
-    private String name; //Имя файла
-    private long size; //Размер в байтах
-    private long compressedSize; //Размер после сжатия в байтах
-    private int compressionMethod; //Метод сжатия
+    private String name;
+    private long size;
+    private long compressedSize;
+    private int compressionMethod;
 
     public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
         this.name = name;
@@ -19,27 +13,6 @@ public class FileProperties {
         this.compressionMethod = compressionMethod;
     }
 
-    /**
-     * будет считать степень сжатия по формуле
-     * @return
-     */
-    public long getCompressionRatio(){
-        return 100 - ((compressedSize * 100) / size);
-    }
-
-    /***/
-    @Override
-    public String toString() {
-        String s = name;
-        if (size > 0){
-            s = String.format("%s %d Kb (%d Kb) сжатие: %d%%",
-                    name,
-                    (size / 1024),
-                    (compressedSize / 1024),
-                    getCompressionRatio());
-        }
-        return s;
-    }
 
     public String getName() {
         return name;
@@ -55,5 +28,28 @@ public class FileProperties {
 
     public int getCompressionMethod() {
         return compressionMethod;
+    }
+
+    public long getCompressionRatio() {
+        // Вычисляем степень сжатия
+        return 100 - ((compressedSize * 100) / size);
+    }
+
+    @Override
+    public String toString() {
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
+        if (size > 0) {
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
+        }
+
+        return builder.toString();
     }
 }
--- ZipFileManager.java
+++ ZipFileManager.java
@@ -1,13 +1,17 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -68,5 +72,32 @@ public class ZipFileManager {
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
         }
+    }
+
+    /**
+     * Будет возвращать список файлов в архиве,
+     * вернее список свойств этих файлов
+     * @return
+     * @throws Exception
+     */
+    public List<FileProperties> getFilesList() throws Exception {
+        if (!Files.isRegularFile(zipFile)) throw new WrongZipFileException();
+        List<FileProperties> filePropertiesList = new ArrayList<>(); //в него мы будем складывать свойства файлов
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null){
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                copyData(zipInputStream, byteArrayOutputStream);
+
+                String name = zipEntry.getName();
+                long size = byteArrayOutputStream.size();
+                long compressedSize = zipEntry.getCompressedSize();
+                int compressionMethod = zipEntry.getMethod();
+
+                filePropertiesList.add(new FileProperties(name, size, compressedSize, compressionMethod));
+            }
+        }
+        return filePropertiesList;
     }
 }
